5799a4671eef02cb84b6f9b231862a05c7a74552
Gregory <lacrosselogo16@gmail.com>
2022-10-21 00:24:56 -0600

    First Hello World, I removed using system statement

---
+++ hello.cs
@@ -0,0 +1,10 @@
+
+class Hello 
+{         
+	static void Main(string[] args)
+        {
+		System.Console.Clear();
+		System.Console.WriteLine("Hello World!");
+		System.Console.Beep();
+        }
+}
