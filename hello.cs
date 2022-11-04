4418ce70266f5e90bdf71b391823d1b5d5648fa3
Gregory <lacrosselogo16@gmail.com>
2022-11-04 11:04:55 -0600

    fix bug when no arguments are passed into Hello argument.

--- hello.cs
+++ hello.cs
@@ -2,13 +2,19 @@ using System;
 namespace blah
 {
 
-class Hello 
-{         
-	static void Main()
-        {
-		Console.Clear();
-		Console.WriteLine("Hello World!");
-		Console.Beep();
-        }
-}
+	class Hello 
+	{         
+		static void Main(string[] args)
+        	{
+			Console.Clear();
+			
+			if(args.Length > 0)
+			{
+				Console.WriteLine(args[0]);
+			}
+			
+			Console.WriteLine("Hello World!");
+			Console.Beep();
+        	}
+	}
 }
