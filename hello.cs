0f31519f689bebd2dad3dcebbb331689e224742a
Gregory <lacrosselogo16@gmail.com>
2022-11-04 11:34:12 -0600

    added french parameter.

--- hello.cs
+++ hello.cs
@@ -3,7 +3,13 @@ namespace blah
 {
 
 	class Hello 
-	{         
+	{       
+		/*
+			want to test four conditions: no parameters, single parameter name, pass in string with spaces, languages none and french, 
+			defaults to english if unknown second parameter with name.
+
+			Example: hello Gregory French
+		*/  
 		static void Main(string[] args)
         	{
 			Console.Clear();
@@ -12,8 +18,17 @@ namespace blah
 			{
 				Console.WriteLine(args[0]);
 			}
-			
-			Console.WriteLine("Hello World!");
+			if(args.Length > 1)
+			{
+				if(args[1] == "French")
+				{
+					Console.WriteLine("Bonjour le monde!");
+				}
+			}
+			else
+			{
+				Console.WriteLine("Hello World!");
+			}
 			Console.Beep();
         	}
 	}
