136d6346c0aa467068a50ad089dabd6519201c56
Gregory <lacrosselogo16@gmail.com>
2022-11-23 12:34:45 -0700

    max refactoring, simplistic now.

--- hello.cs
+++ hello.cs
@@ -5,112 +5,53 @@ namespace blah
 	// Returns Name if specified and Hello World in many different language that you specify.
 	class Hello 
 	{
-		static string value1 = "";
-    	static string value2 = "";
-    	// static string _name = "";
-    	// static string _language = "";
-    	// static string _greeting = "";
-    	
     	static Dictionary<string,string> _mapGreetings = new Dictionary<string,string>{{"ENGLISH", "Hello World!"},{"FRENCH", "Bonjour le monde!"},{"ITALIAN", "Ciao Mondo!"},{"SPANISH", "Hola Mundo!"},{"JAPANESE", "こんにちは世界"},{"CHINESE", "你好世界"}};
+    	static string _name = "";
+    	static string _greeting = _mapGreetings["ENGLISH"];
+    	
 
 		static void Main(string[] args)
         {
         	Intialize(args);	
-
-        	// one parameter logic
-			DisplayLanguageGreeting(value1);
-
-			// two parameter logic
-			if(IsNotAnyLanguage(value1))
-			{
-				DisplayLanguageGreeting(value2);
-			}
-
-			// Not equal to 2 ege 3 or more or 1 or none
-			if(ShouldDisplayDefaultGreeting(args))
-			{
-				DisplayDefaultGreeting();
-			}
+        	DisplayGreeting();
     	}
 
     	static void Intialize(string[] args)
     	{
-    		
-        	if(args.Length > 0)
-        	{
-        		value1 = args[0];
-        		if(args.Length > 1)
-        		{
-        			value2 = args[1];
-        		}
-        	}
-        	DisplayInitialGreeting(value1, value2);
+    		if(args.Length > 0)
+    		{
+    			if(IsAMappedLanguage(args[0]))
+    			{
+
+    			}
+    			else
+    			{
+    				_name = args[0];
+    				if(args.Length > 1 && IsAMappedLanguage(args[1]))
+    				{
+
+    				}
+    			}
+    		}
     	}
 
-    	// static void DisplayLanguages()
-    	// {
-    	// 	for(int index = 0; index < _languages.Length; index++)
-    	// 	{
-    	// 		Console.Write(_languages[index]);
-    	// 		Console.Write(" ");
-    	// 		Console.WriteLine(_greetings[index]);
-    	// 	}
-    	// }
+    	static void DisplayGreeting()
+    	{
+    		Console.WriteLine(_name);
+    		Console.WriteLine(_greeting);
+    	}
 
-    	static void DisplayLanguageGreeting(string language)
+		static bool IsAMappedLanguage(string language)
     	{
     		try
     		{
-    			Console.WriteLine(_mapGreetings[language.ToUpper()]);
-    		}
-    		catch
-    		{
-    			Console.WriteLine(_mapGreetings["ENGLISH"]);
-    		}
-    	}
-
-    	static bool ShouldDisplayDefaultGreeting(string[] args)
-    	{
-    		return (args.Length != 2 && ( args.Length == 0 || ( args.Length == 1 && IsNotAnyLanguage(args[0]))));
-    	}
-
-    	//check if string is NOT a mapped language.
-		static bool IsNotAnyLanguage(string language)
-    	{
-    		try
-    		{
-    			var message = _mapGreetings[language.ToUpper()];
-    			return false;
-    		}
-    		catch
-    		{
+    			_greeting = _mapGreetings[language.ToUpper()];
     			return true;
     		}
-    	}
-    		
-    	static void DisplayInitialGreeting(string val1, string val2)
-    	{
-    		if(IsNotAnyLanguage(val1))
-        	{
-	        	if(val1 != "")
-	        	{
-	        		Console.WriteLine(val1);
-	        	}
-	        	if(val2 != "" && IsNotAnyLanguage(val2))
-	        	{
-	        		DisplayDefaultGreeting();	
-	        	}
-        	}
-    	}
-
-    	static void DisplayDefaultGreeting()
-    	{
-			DisplayEnglishGreeting();
-    	}
-
-    	static void DisplayEnglishGreeting()
-    	{
-			Console.WriteLine("Hello World!");
+    		catch
+    		{
+    			return false;
+    		}
     	}
 	}
 }
