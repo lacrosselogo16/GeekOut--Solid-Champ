749167e029d512076147fbcf65f6e2d757fc1d2b
Gregory <lacrosselogo16@gmail.com>
2022-12-22 13:00:17 -0700

    add rest of my create test for factory.

--- Tests/ArgFactory.cs
+++ Tests/ArgFactory.cs
@@ -2,32 +2,42 @@ namespace Tests
 {
 	public enum ArgType
 	{
-		LANGUAGE_ONLY
+		NULL
+		,LANGUAGE_ONLY
 		,LANGUAGE_ONLY_UPPERCASE
+		,LANGUAGE_ONLY_LOWERCASE
+		,LANGUAGE_ONLY_MIXEDCASE
+		,FIRSTNAME_WITH_LANGUAGE
+		,FULLNAME_WITH_LANGUAGE
+		,FIRSTNAME_LANGUAGE_AND_THIRD_PARAMETER
 	}
 	public class ArgFactory:GreetingTestsBase
 	{
 		protected string[] Create(ArgType value)
 		{
-			string[] result;
 			switch(value)
 			{
 				case ArgType.LANGUAGE_ONLY:
-            		result = new string[] {_languageName};
-					break;
+            		return new string[] {_languageName};
 				case ArgType.LANGUAGE_ONLY_UPPERCASE:
-	            	result = new string[] {_languageName.ToUpper()};
-					break;
-				default:
-	            	result = new string[] {_languageName.ToLower()};
-	            	result = new string[] {_userName,_languageName};
-	            	result = new string[] {_fullName,_languageName};
-	            	result = new string[] {_userName,_languageName, "blah"};
-	            	result = new string[] {TestHelper.MixedCase(_languageName)};
-					break;
-			}
+	            	return new string[] {_languageName.ToUpper()};
+				case ArgType.LANGUAGE_ONLY_LOWERCASE:
+	            	return new string[] {_languageName.ToLower()};
+				case ArgType.LANGUAGE_ONLY_MIXEDCASE:
+	            	return new string[] {TestHelper.MixedCase(_languageName)};
 
-			return result;
+	            case ArgType.FIRSTNAME_WITH_LANGUAGE:
+	            	return new string[] {_userName,_languageName};
+	
+				case ArgType.FULLNAME_WITH_LANGUAGE:
+	            	return new string[] {_fullName,_languageName};
+	
+				case ArgType.FIRSTNAME_LANGUAGE_AND_THIRD_PARAMETER:
+	            	return new string[] {_userName,_languageName, "blah"};
+				case ArgType.NULL:
+	            default:
+	            	return null;
+			}
 		}
 	}
 }
