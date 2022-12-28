c0e44d3d6706d352b1ee719945b2f1ec07b6add3
Gregory <lacrosselogo16@gmail.com>
2022-12-28 12:44:26 -0700

    add oneOff test, complete and passing.

--- Tests/GreetingOneOffTest.cs
+++ Tests/GreetingOneOffTest.cs
@@ -4,7 +4,7 @@ using HelloWorldProgram.BusinessLogic;
 namespace Tests
 {
     [TestClass]
-    public class GreetingOneOffTest
+    public class GreetingOneOffTest:GreetingTestsBase
     {
         [TestMethod]
         public void BlankArgsTest()
@@ -12,6 +12,32 @@ namespace Tests
             //Arrange
             var expected = "Hello World!";
             string[] args = {""};
+
+            //Act
+            var actual = GreetingFactory.Create(args).Display();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void FirstNameOnlyTest()
+        {
+            //Arrange
+            var expected = ExpectedGreetingWith();
+            string[] args = {_userName};
+
+            //Act
+            var actual = GreetingFactory.Create(args).Display();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void FullNameOnlyTest()
+        {
+            //Arrange
+            var expected = ExpectedGreetingWith(true);
+            string[] args = {_fullName};
 
             //Act
             var actual = GreetingFactory.Create(args).Display();
