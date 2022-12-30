c78d63639b690913d705d5d7374838048ebe5601
Gregory <lacrosselogo16@gmail.com>
2022-12-30 11:54:46 -0700

    add assembly info.

---
+++ AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("My Hello EXE file")]
+[assembly: AssemblyCompany("Murder Co.")]
+[assembly: AssemblyDescription("Hello World")]
+[assembly: AssemblyVersion("3.0.0.1")]
+[assembly: AssemblyFileVersion("3.0.0.2")]
--- BusinessLogic/AssemblyInfo.cs
+++ BusinessLogic/AssemblyInfo.cs
@@ -1,2 +1,9 @@
+using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 [assembly: InternalsVisibleTo("GreetingTests")]
+[assembly: AssemblyTitle("This is Just A Test Title")]
+[assembly: AssemblyCompany("Murder Co.")]
+[assembly: AssemblyDescription("Hello World, Im coming for you!")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
